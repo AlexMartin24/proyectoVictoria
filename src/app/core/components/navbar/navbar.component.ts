6b4f8667f751556163cc774d42000d1bc4fd3b0e
Martin <alexmpalomo64@gmail.com>
2025-11-26 00:39:41 -0300

    se agregan url al navbar. Se coloca image not found a los restaurant

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,44 +1,43 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { Observable } from 'rxjs';
 import { SharedModule } from '../../../shared/shared.module';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginDialogComponent } from '../../../auth/components/login-dialog/login-dialog.component';
 import { AuthService } from '../../../auth/services/auth.service';
+import { User } from '../../../users/model/user.model';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
 
   isLoggedIn$!: Observable<boolean>;
+  currentUser: User | null = null; // permite null para evitar errores
 
-  constructor(private dialog: MatDialog,
-    private router: Router, private loginService: AuthService) { }
+  constructor(
+    private dialog: MatDialog,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
-    this.isLoggedIn$ = this.loginService.isLoggedIn$;
+    this.isLoggedIn$ = this.authService.isLoggedIn$;
+
+    this.authService.currentUser$.subscribe(user => {
+      this.currentUser = user; // puede ser null
+    });
   }
 
   logOut() {
-    this.loginService
-      .logout()
-      .then(() => {
-        this.router.navigate(['/']);
-
-      })
-      .catch((error) => console.log(error));
+    this.authService.logout()
+      .then(() => this.router.navigate(['/']))
+      .catch(error => console.log(error));
   }
-
-  redirectToLogin() {
-    this.router.navigate(['/auth/iniciar-sesion']);
-  }
-
-
 
   loginDialog() {
     const dialogRef = this.dialog.open(LoginDialogComponent, {
@@ -47,9 +46,12 @@ export class NavbarComponent {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      // console.log('El diálogo fue cerrado', result);
+      // Se puede manejar algo después de cerrar el diálogo
     });
   }
 
-}
+  goToUserProfile(){
+    
+  }
 
+}
